5cd796b4c530763c9cca75f93e404cb8daad3707
Muskanjha-20 <muskanjha233@gmail.com>
2024-04-03 22:46:36 +0530

    first_commit

---
+++ lib/consts.dart
@@ -0,0 +1,4 @@
+const String NEWS_API_KEY = "dd76fb946d9b4572990dc4381913d9dc";
+
+const String PLACEHOLDER_IMAGE_LINK =
+    "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png";
